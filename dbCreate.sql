0c958845751e6ca30e47b04df36a752abbfb6d72
Sofía Gacitúa <140858102+Sof-1215@users.noreply.github.com>
2024-10-20 23:52:27 -0300

    Creacion DB y tablas producto y categoria

---
+++ dbCreate.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE ecommerce;
+\c
+--Creacion tabla cliente
+CREATE TABLE cliente(
+    id_cliente SERIAL PRIMARY KEY,
+    nombre VARCHAR(255),
+    direccion VARCHAR(255),
+    email VARCHAR(100),
+    telefono VARCHAR(20)
+);
+
+--Crear tabla categoria
+CREATE TABLE categoría (
+	id_categoria SERIAL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL
+);
+
+--Crear tabla producto
+CREATE TABLE producto (
+	id_producto SERIAL PRIMARY KEY,
+	nombre VARCHAR(255) NOT NULL,
+	descripción TEXT,
+	precio DECIMAL(10, 2),
+	stock INT NOT NULL,
+	estado VARCHAR(50) NOT NULL,
+	id_categoria INTEGER,
+	FOREIGN KEY (id_categoria) REFERENCES categoría (id_categoria)
+);
+
+
